b7eaf07c244088fb53c72e252a5f0570504c8168
Leonardo-Akira95 <59270176+Leonardo-Akira95@users.noreply.github.com>
2020-01-09 18:14:41 -0300

    Add files via upload

---
+++ app/js/script.js
@@ -0,0 +1,104 @@
+    
+    $(function() {
+
+        var chartData = [];
+
+        /* $.ajax({
+            type: 'GET',
+            url: 'http://localhost/rest/api/leituras/read.php',
+            async:false,
+            success: function(leituras) {
+                $.each(leituras, function(index, leitura) {
+                    leiturasDatas[index] = leitura.data_criada;
+                    leiturasValores[index] = leitura.valor;
+                    console.log(leiturasDatas[index]);
+                    console.log(leiturasValores[index]);
+                })
+            }
+        }); */
+
+        function getData(){
+            $.getJSON( "http://localhost/rest/api/leituras/read.php", function( data ) {
+            
+                Highcharts.each(data, function(leitura){
+                    leitura.x = new Date(leitura.data_criada).getTime();
+                  leitura.y = Number(leitura.valor);
+                    chartData.push(leitura);
+                });
+                chartData.sort(function(a, b){
+                    return a.x - b.x
+                });
+                  chart.series[0].setData(chartData);
+            });
+        }
+
+        var chart = Highcharts.chart('container', {
+            chart: {type: 'spline'},
+            title: {
+                text: ''
+            },
+        
+            subtitle: {
+                text: 'Source: thesolarfoundation.com'
+            },
+        
+            yAxis: {
+                title: {
+                    text: 'Valores de leituras'
+                }
+            },
+        
+            xAxis: {
+                title: {
+                    text: 'Data de leitura'
+                },
+                type: 'datetime',
+                accessibility: {
+                    rangeDescription: 'Range: 2010 to 2017'
+                }
+            },
+        
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+            },
+        
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false
+                    },
+                    pointStart: 2010
+                }
+            },
+        
+            series: [{
+                name: 'Datas de leitura',
+                data: []
+            }, {
+                name: 'Valores de leitura',
+                data: []
+            }],
+        
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        legend: {
+                            layout: 'horizontal',
+                            align: 'center',
+                            verticalAlign: 'bottom'
+                        }
+                    }
+                }]
+            }
+        
+        });
+
+        getData();
+    });
+
+    
